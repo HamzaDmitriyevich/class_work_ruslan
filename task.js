e5e5e56e2dda70ad1fadfe821a0a94f3f0e97e16
Ruslan <krou1937@gmail.com>
2023-12-19 21:06:29 +0300

    Работа со скриптом

--- task.js
+++ task.js
@@ -1,1 +1,6 @@
-console.log('Скрипт работает');
+function calc_age(year_of_birth) {
+    let dt = new Date();
+    var result = dt.getFullYear() - year_of_birth;
+    age.innerHTML = result;
+	return result;
+}
