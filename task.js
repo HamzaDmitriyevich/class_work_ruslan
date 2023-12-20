1c611703aebb343302f8606d4016e1ac7d8f8488
HamzaDmitriyevich <krou1937@gmail.com>
2023-12-20 22:37:35 +0300

    Исправил несоответствие перемменых с html

--- task.js
+++ task.js
@@ -1,10 +1,21 @@
 function checkWeightHeight(){
-    var weight = document.getElementById("weight").value;
-    var height = document.getElementById("height").value;
+    var weight = document.getElementById("ves").value;
+    var height = document.getElementById("rost").value;
 
     var ras = weight / (height*height);
     var result ="";
 
+    if (ras < 55){
+        result = "Дрищ";
+    }else if (ras >= 55 && ras < 85){
+        result= "Норм вес";
+    }else if (ras >= 88 && ras < 130){
+        result = "Вы Жирненький";
+    }else{
+        result = "Ну это слишком"
+    }
+    
+    document.getElementById("result").innerHTML =result;
     
 
 
