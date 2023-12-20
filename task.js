50e5a3e866420f8a333738bcf23874adf9d94979
HamzaDmitriyevich <krou1937@gmail.com>
2023-12-20 22:15:16 +0300

    Добавил функцию вjs

--- task.js
+++ task.js
@@ -1,1 +1,11 @@
+function checkWeightHeight(){
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
 
+    var ras = weight / (height*height);
+    var result ="";
+
+    
+
+
+}
